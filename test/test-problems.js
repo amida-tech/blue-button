71008a73ff7a720d4fa1207695e59e6ec7a21b83
Matthew McCall <matt@mattmccall.com>
2014-05-06 13:43:17 -0700

    incremental cleanup commit.

--- test/test-problems.js
+++ test/test-problems.js
@@ -1,5 +1,8 @@
-var expect = require('chai').expect;
-var assert = require('chai').assert;
+var chai = require('chai');
+chai.use(require('chai-datetime'));
+var expect = chai.expect;
+var assert = chai.assert;
+
 
 var fs = require('fs');
 var path = require('path');
@@ -7,13 +10,31 @@ var path = require('path');
 var bb = require('../index');
 var jsutil = require('../lib/jsutil');
 
-describe('problems parser', function() {
+var problems;
+
+var loadRecord = function(done) {
+    var filepath = path.join(__dirname, 'fixtures/file-snippets/CCD_1_Problems.xml');
+    var xml = fs.readFileSync(filepath, 'utf-8');
+    bb.parse(xml, {
+        component: 'ccda_problems'
+    }, function(err, result) {
+        problems = result.toJSON();
+        jsutil.deepDelete(problems, '_id');
+        done();
+    });
+}
+
+
+
+xdescribe('problems parser', function() {
     var problems = null;
-    
+
     before(function(done) {
-        var filepath  = path.join(__dirname, 'fixtures/file-snippets/CCD_1_Problems.xml');
+        var filepath = path.join(__dirname, 'fixtures/file-snippets/CCD_1_Problems.xml');
         var xml = fs.readFileSync(filepath, 'utf-8');
-        bb.parse(xml, {component: 'ccda_problems'}, function(err, result) {
+        bb.parse(xml, {
+            component: 'ccda_problems'
+        }, function(err, result) {
             problems = result.toJSON();
             jsutil.deepDelete(problems, '_id');
             //var json2Write = JSON.stringify(ccd, undefined, '\t');
@@ -22,17 +43,17 @@ describe('problems parser', function() {
             done();
         });
     });
-    
+
     xit('full deep check', function(done) {
         expect(problems).to.exist;
-        var filepath  = path.join(__dirname, 'fixtures/file-snippets/json/CCD_1_Problems.json');
+        var filepath = path.join(__dirname, 'fixtures/file-snippets/json/CCD_1_Problems.json');
         var json2Read = fs.readFileSync(filepath, 'utf-8');
         var expectedProblems = jsutil.jsonParseWithDate(json2Read);
         expect(problems).to.deep.equal(expectedProblems);
         done();
     });
-    
-    it ('spot check', function(done) {
+
+    xit('spot check', function(done) {
         console.log(JSON.stringify(problems, null, 4));
         expect(problems).to.exist;
         expect(problems.problemConcerns).to.exist;
@@ -47,7 +68,143 @@ describe('problems parser', function() {
         expect(problems.problemConcerns[1].problems[0].dateRange.low).to.exist;
         expect(JSON.stringify(problems.problemConcerns[1].problems[0].dateRange.low)).to.equal('"2007-01-03T00:00:00.000Z"');
         expect(problems.problemConcerns[1].problems[0].dateRange.lowResolution).to.equal('day');
-        
+
         done();
     });
+});
+
+describe('Problems - Schema Conformance', function() {
+
+    before(function(done) {
+        if (problems === undefined) {
+            loadRecord(function() {
+                done();
+            });
+        } else {
+            done();
+        }
+    });
+
+    it('Basic Object Structure', function(done) {
+        assert.isObject(problems, 'Main item should be object');
+        assert.isArray(problems.problems, 'Sub object should be array.');
+        done();
+
+    });
+
+    it('Problem Structure - Date', function(done) {
+        for (var i in problems.problems) {
+            var currentProblem = problems.problems[i];
+            assert.isDefined(currentProblem.date, 'Date should exist');
+            assert.isArray(currentProblem.date, 'Date should be an array');
+            assert.ok(currentProblem.date.length < 3, 'Date optional, max length of two');
+
+            for (var ii in currentProblem.date) {
+                var currentDate = currentProblem.date[ii];
+                assert.ok(currentDate.date.getDate(), 'Date should be parseable');
+                assert.isString(currentDate.precision, 'Precision should be a string');
+                assert.ok(currentDate.precision.length > 0, 'Precision should have content');
+                assert.includeMembers(['year', 'month', 'day', 'hour', 'minute', 'second', 'subsecond'], new Array(currentDate.precision), 'Precision should be valid');
+            }
+        };
+        done();
+    });
+
+    it('Problem Structure - Identifiers', function(done) {
+        for (var i in problems.problems) {
+            var currentProblem = problems.problems[i];
+            assert.isDefined(currentProblem.identifiers, 'Identifier should exist');
+            assert.isArray(currentProblem.identifiers, 'Identifier should be an array');
+            assert.ok(currentProblem.identifiers.length > 0, 'Identifier required.');
+
+            for (var ii in currentProblem.identifiers) {
+                var currentId = currentProblem.identifiers[ii];
+                assert.isString(currentId.identifier, 'Identifier should be a string');
+                assert.ok(currentId.identifier.length > 0, 'Identifier should have content');
+            }
+        }
+        done();
+    });
+
+    it('Problem Structure - Identifiers', function(done) {
+        for (var i in problems.problems) {
+            var currentProblem = problems.problems[i];
+            assert.isDefined(currentProblem.identifiers, 'Identifier should exist');
+            assert.isArray(currentProblem.identifiers, 'Identifier should be an array');
+            assert.ok(currentProblem.identifiers.length > 0, 'Identifier required.');
+
+            for (var ii in currentProblem.identifiers) {
+                var currentId = currentProblem.identifiers[ii];
+                assert.isString(currentId.identifier, 'Identifier should be a string');
+                assert.ok(currentId.identifier.length > 0, 'Identifier should have content');
+                //TODO:  Add assertions if identifier_type comes in, and snippets to test.
+            }
+        }
+        done();
+    });
+
+    it('Problem Structure - Negation', function(done) {
+        for (var i in problems.problems) {
+            var currentProblem = problems.problems[i];
+            //TODO:  Add negation indication checking.
+        }
+        done();
+    });
+
+    it('Problem Structure - Name', function(done) {
+        for (var i in problems.problems) {
+            var currentProblem = problems.problems[i];
+            assert.isDefined(currentProblem.name, 'Name should exist');
+            assert.isString(currentProblem.name, 'Name should be a string');
+            assert.ok(currentProblem.name.length > 0, 'Name required.');
+        }
+        done();
+    });
+
+    it('Problem Structure - Code', function(done) {
+        for (var i in problems.problems) {
+            var currentProblem = problems.problems[i];
+            assert.isDefined(currentProblem.code, 'Code should exist');
+            assert.isString(currentProblem.code, 'Code should be a string');
+            assert.ok(currentProblem.code.length > 0, 'Code required.');
+        }
+        done();
+    });
+
+    it('Problem Structure - Code system', function(done) {
+        for (var i in problems.problems) {
+            var currentProblem = problems.problems[i];
+            assert.isDefined(currentProblem.code_system, 'Code should exist');
+            assert.isString(currentProblem.code_system, 'Code should be a string');
+            assert.ok(currentProblem.code_system.length > 0, 'Code required.');
+            assert.includeMembers(['SNOMED CT'], new Array(currentProblem.code_system), 'System should be valid coding system');
+        }
+        done();
+    });
+
+    it('Problem Structure - Onset Age', function(done) {
+        for (var i in problems.problems) {
+            var currentProblem = problems.problems[i];
+            if (currentProblem.onset_age) {
+                assert.isString(currentProblem.onset_age, 'Age should be a string');
+                assert.ok(Number(currentProblem.onset_age) !== "NaN", 'Age should be a number');
+                assert.isDefined(currentProblem.onset_age_unit, 'Age should have units with it');
+            }
+        }
+        done();
+    });
+
+    it('Problem Structure - Onset Age Unit', function(done) {
+        for (var i in problems.problems) {
+            var currentProblem = problems.problems[i];
+            if (currentProblem.onset_age_unit) {
+                assert.isDefined(currentProblem.onset_age_unit, 'Unit should have age with it');
+                assert.isString(currentProblem.onset_age_unit, 'Age Units should be a string');
+                assert.includeMembers(['Year', 'Month', 'Day', 'Week', 'Minute', 'Hour', 'Second', 'Subsecond'], new Array(currentProblem.onset_age_unit), 'Age unit should be known value');
+            }
+        }
+        done();
+    });
+
+
 });
