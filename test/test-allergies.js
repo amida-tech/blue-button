be9f8ef501b7ed55028110b7bfd35964723cf8f7
au2 <afsin@amida-tech.com>
2014-05-06 14:48:10 -0400

    First version of allergies without the extract fields cleanup.

---
+++ test/test-allergies.js
@@ -0,0 +1,55 @@
+var expect = require('chai').expect;
+var assert = require('chai').assert;
+
+var fs = require('fs');
+var path = require('path');
+
+var bb = require('../index');
+var jsutil = require('../lib/jsutil');
+
+describe('allergies parser', function() {
+    var allergies = null;
+    
+    before(function(done) {
+        var filepath  = path.join(__dirname, 'fixtures/file-snippets/CCD_1_Allergies.xml');
+        var xml = fs.readFileSync(filepath, 'utf-8');
+        bb.parse(xml, {component: 'ccda_allergies'}, function(err, result) {
+            allergies = result.toJSON();
+            jsutil.deepDelete(allergies, '_id');
+            //var json2Write = JSON.stringify(allergies, undefined, '\t');
+            //var jsonFilePath = filepath.replace('file-snippets', 'file-snippets/json');
+            //jsonFilePath = jsonFilePath.replace('.xml', '.json');
+            //fs.writeFileSync(jsonFilePath, json2Write);
+            done();
+        });
+    });
+    
+    it('full deep check', function(done) {
+        expect(allergies).to.exist;
+        var filepath  = path.join(__dirname, 'fixtures/file-snippets/json/CCD_1_Allergies.json');
+        var json2Read = fs.readFileSync(filepath, 'utf-8');
+        var expectedAllergies = jsutil.jsonParseWithDate(json2Read);
+        expect(allergies).to.deep.equal(expectedAllergies);
+        done();
+    });
+    
+    it ('spot check', function(done) {
+        expect(allergies).to.exist;
+        expect(allergies.problemAct).to.exist;
+        expect(allergies.problemAct).to.have.length(3);
+        
+        expect(allergies.problemAct[1].dateRange).to.exist;
+        expect(allergies.problemAct[1].dateRange.point).to.exist;
+
+        expect(JSON.stringify(allergies.problemAct[1].dateRange.point)).to.equal('"2006-05-01T00:00:00.000Z"');
+
+        expect(allergies.problemAct[1].observation).to.exist;
+        expect(allergies.problemAct[1].observation.severity).to.equal('Moderate');
+        expect(allergies.problemAct[1].observation.status).to.equal('Active');
+
+        expect(allergies.problemAct[1].observation.allergen).to.exist;
+        expect(allergies.problemAct[1].observation.allergen.label).to.equal('Codeine');
+        
+        done();
+    });
+});
