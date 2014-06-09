b812705d3e5ad60c854b250890b6ee5953a0f1b2
Dmitry Kachaev <dmitry.kachaev@gmail.com>
2014-06-09 08:23:22 -0400

    Merged BJ's code into CMS parser lib and added simple test

---
+++ test/test-cms-parser.js
@@ -0,0 +1,28 @@
+var expect = require('chai').expect;
+var fs = require('fs');
+var path = require('path');
+
+var cms = require('../lib/parser/cms/cmsParser.js');
+
+var txtdata;
+describe('CMS parser.js', function() {
+    
+    before(function(done) {
+        var filepath  = path.join(__dirname, 'fixtures/cms/sample2.txt');
+
+        console.log(filepath);
+        var txtfile = fs.readFileSync(filepath, 'utf-8');
+
+        txtdata=txtfile.toString();
+        done();
+    });
+    
+    it('check valid data parsing', function(done) {
+        var result = cms(txtdata);
+
+        //console.log(result);
+        expect(result).to.exist;
+        done();
+    });
+    
+});
