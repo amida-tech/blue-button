84b344152bd885ce89777fea1b9b5a04a1c5f4f4
BJ Shin <bs5ht@virginia.edu>
2014-06-30 00:02:54 -0400

    added jsonschema drafts for claims and insruance

--- example/example3.js
+++ example/example3.js
@@ -1,8 +1,10 @@
 var validator = require("../index.js").validator;
 var fs = require("fs");
+var bb = JSON.parse(fs.readFileSync(__dirname + '/CCD1.json', 'utf8'));
+var data = fs.readFileSync("CCD_1.xml").toString();
 
-var bb = JSON.parse(fs.readFileSync(__dirname + '/CCD1.json', 'utf8').toString());
-
+//parse xml into JS object
+console.log(typeof bb);
 //validator.setup;
 
 var result = validator.validateDocumentModel(bb);
