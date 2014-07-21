38207df2a782b0f8065dd8708528dcf6aa982069
BJ Shin <bs5ht@virginia.edu>
2014-07-21 16:18:24 -0400

    updated parser to accomdate Jones mock up, fixed some bugs in various areas

--- test/test-parsers-validation.js
+++ test/test-parsers-validation.js
@@ -12,17 +12,35 @@ describe('Test CMS Parsing, from sample file', function () {
     });
     it('find bad entries', function (done) {
         var obj = bb.parseText(sampleFile);
-        //console.log(JSON.stringify(obj, null, 4));
+        //console.log(JSON.stringify(obj.data.results, null, 4));
         var valid = validator.validateDocumentModel(obj);
         if (!valid) {
             var errors = validator.getLastError();
-            console.log(errors);
             for (var x in errors.errors) {
                 console.log(errors.errors[x]);
             }
         }
         expect(valid).to.be.true;
+        done();
+    });
 
+});
+
+describe('Test CMS Parsing, isabella jones cms version', function () {
+    before(function (done) {
+        sampleFile = fs.readFileSync(__dirname + '/fixtures/cms/jones.cms.txt').toString();
+        done();
+    });
+    it('find bad entries', function (done) {
+        var obj = bb.parseText(sampleFile);
+        var valid = validator.validateDocumentModel(obj);
+        if (!valid) {
+            var errors = validator.getLastError();
+            for (var x in errors.errors) {
+                console.log(errors.errors[x]);
+            }
+        }
+        expect(valid).to.be.true;
         done();
     });
 
