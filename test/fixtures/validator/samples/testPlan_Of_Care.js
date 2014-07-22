3effceb26e83283afbf076f0c23aa8b8ac41eb97
Kevin Young <kyoung18@terpmail.umd.edu>
2014-07-22 16:40:07 -0400

    test for validator passing

---
+++ test/fixtures/validator/samples/testPlan_Of_Care.js
@@ -0,0 +1,82 @@
+var test_plan_of_care_list = {
+	"regular1": 
+		[{
+        "date_time": {
+            "point": {
+                    "date": "2012-05-12T00:00:00Z",
+                    "precision": "day"
+                }
+        },
+        "identifiers": [
+            {
+                "identifier": "9a6d1bac-17d3-4195-89a4-1121bc809b4a"
+            }
+        ],
+        "plan": {
+            "name": "Colonoscopy",
+            "code": "73761001",
+            "code_system_name": "SNOMED CT"
+        },
+        "type": "observation"
+    },
+    {
+        "date_time": {
+            "point": {
+                    "date": "2012-05-12T00:00:00Z",
+                    "precision": "day"
+                }
+        },
+        "identifiers": [
+            {
+                "identifier": "9a6d1bac-17d3-4195-89a4-1121bc809a5c"
+            }
+        ],
+        "plan": {
+            "name": "Colonoscopy",
+            "code": "73761001",
+            "code_system_name": "SNOMED CT"
+        },
+        "type": "act"
+    },
+    {
+        "date_time": {
+            "point": {
+                    "date": "2012-05-12T00:00:00Z",
+                    "precision": "day"
+                }
+        },
+        "identifiers": [
+            {
+                "identifier": "9a6d1bac-17d3-4195-89a4-1121bc809b4d"
+            }
+        ],
+        "plan": {
+            "name": "Colonoscopy",
+            "code": "73761001",
+            "code_system_name": "SNOMED CT"
+        },
+        "type": "encounter"
+    },
+    {
+        "date_time": {
+            "point": {
+                    "date": "2012-05-12T00:00:00Z",
+                    "precision": "day"
+                }
+        },
+        "identifiers": [
+            {
+                "identifier": "9a6d1bac-17d3-4195-89c4-1121bc809b5a"
+            }
+        ],
+        "plan": {
+            "name": "Colonoscopy",
+            "code": "73761001",
+            "code_system_name": "SNOMED CT"
+        },
+        "type": "procedure"
+    }
+]
+}
+
+exports.test_plan_of_care_list = test_plan_of_care_list;
