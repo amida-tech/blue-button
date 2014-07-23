7e66ea20ebd176c6bc87e4ad067a9558f3b8e361
Kevin Young <kyoung18@terpmail.umd.edu>
2014-07-23 17:46:00 -0400

    capitalizing all

--- test/ccda-sections/test-payers.js
+++ test/ccda-sections/test-payers.js
@@ -16,7 +16,7 @@ describe('payers parser', function () {
         payers = bb.parseString(xml, {
             component: 'ccda_payers'
         }).data;
-        var writeFilePath = path.join(__dirname, '../fixtures/file-snippets/generatedJSON/CCD_1_payers.json');
+        var writeFilePath = path.join(__dirname, '../fixtures/file-snippets/generatedJSON/CCD_1_Payers.json');
         fs.writeFileSync(writeFilePath, JSON.stringify(payers, null, 4), 'utf-8');
         done();
     });
@@ -24,7 +24,7 @@ describe('payers parser', function () {
     it('full deep check', function (done) {
         expect(payers).to.exist;
         // console.log(JSON.stringify(payers, null, 10));
-        var filepath = path.join(__dirname, '../fixtures/file-snippets/json/CCD_1_payers.json');
+        var filepath = path.join(__dirname, '../fixtures/file-snippets/json/CCD_1_Payers.json');
         var json2Read = fs.readFileSync(filepath, 'utf-8');
         var expected = jsutil.jsonParseWithDate(json2Read);
         expect(payers).to.deep.equal(expected);
