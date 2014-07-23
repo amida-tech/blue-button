04c03e83546b4acae071eba98cbe02f03ac27b5d
Kevin Young <kyoung18@terpmail.umd.edu>
2014-07-23 17:23:38 -0400

    payers passing completed and passing basic test

--- test/ccda-sections/test-payers.js
+++ test/ccda-sections/test-payers.js
@@ -7,28 +7,28 @@ var path = require('path');
 var bb = require('../../index');
 var jsutil = require('../../lib/jsutil');
 
-// describe('payers parser', function () {
-//     var payers = null;
-//     before(function (done) {
+describe('payers parser', function () {
+    var payers = null;
+    before(function (done) {
 
-//         var filepath = path.join(__dirname, '../fixtures/file-snippets/CCD_1_payers.xml');
-//         var xml = fs.readFileSync(filepath, 'utf-8');
-//         payers = bb.parseString(xml, {
-//             component: 'ccda_payers'
-//         }).data;
-//         var writeFilePath = path.join(__dirname, '../fixtures/file-snippets/generatedJSON/CCD_1_payers.json');
-//         fs.writeFileSync(writeFilePath, JSON.stringify(payers, null, 4), 'utf-8');
-//         done();
-//     });
+        var filepath = path.join(__dirname, '../fixtures/file-snippets/CCD_1_payers.xml');
+        var xml = fs.readFileSync(filepath, 'utf-8');
+        payers = bb.parseString(xml, {
+            component: 'ccda_payers'
+        }).data;
+        var writeFilePath = path.join(__dirname, '../fixtures/file-snippets/generatedJSON/CCD_1_payers.json');
+        fs.writeFileSync(writeFilePath, JSON.stringify(payers, null, 4), 'utf-8');
+        done();
+    });
 
-//     it('full deep check', function (done) {
-//         expect(payers).to.exist;
-//         console.log(JSON.stringify(payers, null, 10));
-//         var filepath = path.join(__dirname, '../fixtures/file-snippets/json/CCD_1_payers.json');
-//         var json2Read = fs.readFileSync(filepath, 'utf-8');
-//         var expected = jsutil.jsonParseWithDate(json2Read);
-//         expect(payers).to.deep.equal(expected);
-//         done();
-//     });
+    it('full deep check', function (done) {
+        expect(payers).to.exist;
+        console.log(JSON.stringify(payers, null, 10));
+        var filepath = path.join(__dirname, '../fixtures/file-snippets/json/CCD_1_payers.json');
+        var json2Read = fs.readFileSync(filepath, 'utf-8');
+        var expected = jsutil.jsonParseWithDate(json2Read);
+        expect(payers).to.deep.equal(expected);
+        done();
+    });
 
-// });
+});
