a700b1ae7e5c772cfd798e0d0a5b8776250474d8
BJ Shin <bs5ht@virginia.edu>
2014-07-24 23:55:25 -0400

    changed schema so that cda_dates date is now object, to match current ccda parser

--- test/test-parsers-validation.js
+++ test/test-parsers-validation.js
@@ -5,6 +5,27 @@ var path = require('path');
 var bb = require('../index.js');
 var validator = require("../index.js").validator;
 
+describe('Test CCDA from sample file', function () {
+    before(function (done) {
+        sampleFile = fs.readFileSync(__dirname + '/fixtures/files/CCD_1.xml').toString();
+        done();
+    });
+    it('find bad entries', function (done) {
+        var obj = bb.parseString(sampleFile);
+        console.log(JSON.stringify(obj.data.results, null, 4));
+        var valid = validator.validateDocumentModel(obj);
+        if (!valid) {
+            var errors = validator.getLastError();
+            for (var x in errors.errors) {
+                console.log(errors.errors[x]);
+            }
+        }
+        expect(valid).to.be.true;
+        done();
+    });
+
+});
+
 describe('Test CMS Parsing, from sample file', function () {
     before(function (done) {
         sampleFile = fs.readFileSync(__dirname + '/fixtures/cms/sample2.txt').toString();
@@ -12,7 +33,7 @@ describe('Test CMS Parsing, from sample file', function () {
     });
     it('find bad entries', function (done) {
         var obj = bb.parseText(sampleFile);
-        //console.log(JSON.stringify(obj.data.results, null, 4));
+        console.log(JSON.stringify(obj.data.results, null, 4));
         var valid = validator.validateDocumentModel(obj);
         if (!valid) {
             var errors = validator.getLastError();
