b415c5c155667da99fa5e74d8a6ff2e890cac4e3
Kevin Young <kyoung18@terpmail.umd.edu>
2014-07-31 19:38:02 -0400

    some modifactions to ccda-r1 parser and adding support for Medication Supply Order entry template within medications

--- test/test-generator.js
+++ test/test-generator.js
@@ -8,7 +8,7 @@ test.verbose = false; // log setting
 
 // testing options/cases
 var TEST_CCDA_SAMPLES = false;
-var TEST_CCD = false;
+var TEST_CCD = true;
 var TEST_SECTIONS = true;
 
 var supportedComponents = {
