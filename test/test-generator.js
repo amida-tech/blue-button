c883a589a0456d529a78df42b5cd7669ff6cdae2
Kevin Young <kyoung18@terpmail.umd.edu>
2014-08-13 22:24:41 -0400

    updating medications

--- test/test-generator.js
+++ test/test-generator.js
@@ -2,6 +2,7 @@ var expect = require('chai').expect;
 var assert = require('chai').assert;
 var lib = require('./test-lib.js');
 var fs = require("fs");
+var bb = require('../index.js');
 
 var test = new lib.testXML();
 test.verbose = true; // log setting
@@ -10,6 +11,7 @@ test.verbose = true; // log setting
 var TEST_CCDA_SAMPLES = false;
 var TEST_CCD = false;
 var TEST_SECTIONS = true;
+var TEST_PARSE_GENERATE = false;
 
 var supportedComponents = {
     payers: 'payers',
@@ -88,7 +90,7 @@ if (TEST_SECTIONS) {
     describe('sections', function () {
         it('should match respective sections', function () {
             Object.keys(supportedComponents).forEach(function (section) {
-                if (section === "vitals") { // add section === "[section]" for specific section
+                if (section === "medications") { // add section === "[section]" for specific section
                     var XMLDOMs = test.generateXMLDOM(section);
 
                     assert.ok(test.isIdentical(XMLDOMs[0].documentElement, XMLDOMs[1].documentElement));
@@ -97,6 +99,45 @@ if (TEST_SECTIONS) {
             });
         });
     });
+}
+
+// test parse->generate->parse->generate
+if (TEST_PARSE_GENERATE) {
+    describe('parse generate parse generate', function () {
+           it('should still be same', function () {
+               var data = fs.readFileSync("test/fixtures/files/CCD_1.xml").toString();
+    
+               //convert string into JSON 
+               var result = bb.parseString(data);
+               // console.log(JSON.stringify(result, null, 4));
+               
+               for (section in result.meta.sections) {
+                   // console.log(result.meta.sections[section], " ", result.data[result.meta.sections[section]].length);
+               }
+               
+               // write generated json
+               fs.writeFileSync("test/fixtures/files/parse_gen_parse/generated/CCD_1_generated.json", JSON.stringify(result, null, 4));
+               
+               // check validation
+               var val = bb.validator.validateDocumentModel(result);
+               // console.log("validation result: ", val);
+               // console.log(JSON.stringify(bb.validator.getLastError(), null, 4));
+               
+               // generate ccda
+               var xml = bb.generateCCDA(result).toString();
+               // write ccda
+               fs.writeFileSync("test/fixtures/files/parse_gen_parse/generated/CCD_1_generated.xml", xml);
+               
+               // parse generated ccda
+               var result2 = bb.parseString(xml);
+               // write the parsed json from the generated ccda
+               fs.writeFileSync("test/fixtures/files/parse_gen_parse/generated/CCD_1_generated_2.json", JSON.stringify(result2, null, 4));
+               
+               // re-generate
+               var xml2 = bb.generateCCDA(result2).toString();
+               fs.writeFileSync("test/fixtures/files/parse_gen_parse/generated/CCD_1_generated_2.xml", xml2);
+           });
+       });
 }
 
 // show the error summary
