2244a24dfd2f9634c73cf2d92cd9757f44ef4480
au2 <afsin@amida-tech.com>
2014-08-28 16:20:00 -0400

    new example for generation,  runs without crashing but fails test

--- test/generator-ccda/test-generator-ccda.js
+++ test/generator-ccda/test-generator-ccda.js
@@ -5,7 +5,7 @@ var fs = require("fs");
 var bb = require('../../index.js');
 
 describe('parse generate parse generate', function () {
-    it('should still be same', function () {
+    it('CCD_1 should still be same', function () {
         var data = fs.readFileSync("./test/fixtures/generator-ccda/CCD_1.xml").toString();
 
         //convert string into JSON 
@@ -30,6 +30,38 @@ describe('parse generate parse generate', function () {
         // re-generate
         var xml2 = bb.generateCCDA(result2).toString();
         fs.writeFileSync("test/fixtures/files/parse_gen_parse/generated/CCD_1_generated_2.xml", xml2);
+
+        delete result.errors;
+        delete result2.errors;
+
+        assert.deepEqual(result2, result);
+    });
+
+    it('Vitera_CCDA_SMART_Sample.xml should still be same', function () {
+        var data = fs.readFileSync("./test/fixtures/generator-ccda/Vitera_CCDA_SMART_Sample.xml").toString();
+
+        //convert string into JSON 
+        var result = bb.parseString(data);
+
+        // write generated json
+        fs.writeFileSync("test/fixtures/files/parse_gen_parse/generated/Vitera_CCDA_SMART_Sample_generated.json", JSON.stringify(result, null, 4));
+
+        // check validation
+        var val = bb.validator.validateDocumentModel(result);
+
+        // generate ccda
+        var xml = bb.generateCCDA(result).toString();
+        // write ccda
+        fs.writeFileSync("test/fixtures/files/parse_gen_parse/generated/Vitera_CCDA_SMART_Sample_generated.xml", xml);
+
+        // parse generated ccda
+        var result2 = bb.parseString(xml);
+        // write the parsed json from the generated ccda
+        fs.writeFileSync("test/fixtures/files/parse_gen_parse/generated/Vitera_CCDA_SMART_Sample_generated_2.json", JSON.stringify(result2, null, 4));
+
+        // re-generate
+        var xml2 = bb.generateCCDA(result2).toString();
+        fs.writeFileSync("test/fixtures/files/parse_gen_parse/generated/Vitera_CCDA_SMART_Sample_generated_2.xml", xml2);
 
         delete result.errors;
         delete result2.errors;
