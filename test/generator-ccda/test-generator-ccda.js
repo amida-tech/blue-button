88a9c1c2d405b9b4383eb10f2f07771d419c55fe
au2 <afsin@amida-tech.com>
2014-09-02 09:57:24 -0400

    Vitera procedures match

--- test/generator-ccda/test-generator-ccda.js
+++ test/generator-ccda/test-generator-ccda.js
@@ -44,7 +44,7 @@ describe('parse generate parse generate', function () {
         var result = bb.parseString(data);
 
         // write generated json
-        fs.writeFileSync("test/fixtures/files/parse_gen_parse/generated/Vitera_CCDA_SMART_Sample_generated.json", JSON.stringify(result.data, null, 4));
+        fs.writeFileSync("test/fixtures/files/parse_gen_parse/generated/Vitera_CCDA_SMART_Sample_generated.json", JSON.stringify(result, null, 4));
 
         // check validation
         var val = bb.validator.validateDocumentModel(result);
@@ -57,7 +57,7 @@ describe('parse generate parse generate', function () {
         // parse generated ccda
         var result2 = bb.parseString(xml);
         // write the parsed json from the generated ccda
-        fs.writeFileSync("test/fixtures/files/parse_gen_parse/generated/Vitera_CCDA_SMART_Sample_generated_2.json", JSON.stringify(result2.data, null, 4));
+        fs.writeFileSync("test/fixtures/files/parse_gen_parse/generated/Vitera_CCDA_SMART_Sample_generated_2.json", JSON.stringify(result2, null, 4));
 
         // re-generate
         var xml2 = bb.generateCCDA(result2).toString();
@@ -65,7 +65,9 @@ describe('parse generate parse generate', function () {
 
         delete result.errors;
         delete result2.errors;
+        delete result.data.social_history;
+        delete result2.data.social_history; // To be fixed.
 
-        assert.deepEqual(result2.data.demographics, result.data.demographics);
+        assert.deepEqual(result2.data, result.data);
     });
 });
