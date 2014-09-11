09d09efb1c2cbb54b650293be6598d02e8f92195
Dmitry Kachaev <dmitry.kachaev@gmail.com>
2014-09-11 17:52:04 -0400

    fixed lint

--- test/sense/test-sense.js
+++ test/sense/test-sense.js
@@ -5,7 +5,7 @@ var fs = require("fs");
 
 var senseString = require("../../index.js").senseString;
 
-describe('sense.js test', function() {
+describe('sense.js test', function () {
     var ccda = "";
     var xml = "";
     var json = "";
@@ -14,7 +14,7 @@ describe('sense.js test', function() {
     var broken_xml = "";
     var va = {};
 
-    before(function() {
+    before(function () {
         ccda = fs.readFileSync('./test/fixtures/sense/CCD.example.xml').toString();
         xml = fs.readFileSync('./test/fixtures/sense/empty.xml').toString();
         xml_no_declaration = fs.readFileSync('./test/fixtures/sense/empty_no_declaration.xml').toString();
@@ -35,48 +35,46 @@ describe('sense.js test', function() {
 
     });
 
-    it('should return NULL for no string with data passed', function() {
+    it('should return NULL for no string with data passed', function () {
         assert.notStrictEqual(undefined, senseString(undefined));
         assert.notStrictEqual(undefined, senseString(null));
         assert.notStrictEqual(undefined, senseString(2013));
     });
 
-    it('should return CCDA for proper CCDA/XML input', function() {
+    it('should return CCDA for proper CCDA/XML input', function () {
         assert.equal('ccda', senseString(ccda).type);
     });
 
-    it('should return XML for proper basic XML input', function() {
+    it('should return XML for proper basic XML input', function () {
         assert.equal('xml', senseString(xml).type);
     });
 
-    it('should return XML for XML input (no <?xml declaration)', function() {
+    it('should return XML for XML input (no <?xml declaration)', function () {
         assert.equal('xml', senseString(xml_no_declaration).type);
     });
 
-
-    it('should return C32 for proper VA C32 XML input', function() {
+    it('should return C32 for proper VA C32 XML input', function () {
         assert.equal('c32', senseString(c32).type);
     });
 
-    it('should return BLUE-BUTTON.JS for proper BB JSON input', function() {
+    it('should return BLUE-BUTTON.JS for proper BB JSON input', function () {
         assert.equal('blue-button.js', senseString(bb_json).type);
     });
 
-
-    it('should return JSON for proper JSON input', function() {
+    it('should return JSON for proper JSON input', function () {
         assert.equal('json', senseString(json).type);
         assert.equal('json', senseString(large_json).type);
     });
 
-    it('should return CMS and version for CMS BB text input', function() {
+    it('should return CMS and version for CMS BB text input', function () {
         assert.equal('cms', senseString(cms).type);
         expect(senseString(cms).version).to.exist;
         assert.equal('2.0', senseString(cms).version);
     });
 
-    it('should return VA and version for VA BB text input', function() {
+    it('should return VA and version for VA BB text input', function () {
         for (v in Object.keys(va)) {
-            var key=Object.keys(va)[v];
+            var key = Object.keys(va)[v];
             //console.log(key);
             var ver = key.split("_");
             if (ver.length === 1) {
@@ -92,19 +90,19 @@ describe('sense.js test', function() {
         }
     });
 
-    it('should return PDF for PDF file input', function() {
+    it('should return PDF for PDF file input', function () {
         assert.equal('pdf', senseString(pdf).type);
     });
 
-    it('should return FORMAT-X for format X text input', function() {
+    it('should return FORMAT-X for format X text input', function () {
         assert.equal('format-x', senseString(format_x).type);
     });
 
-    it('should return UNKNOWN for text input', function() {
+    it('should return UNKNOWN for text input', function () {
         assert.equal('unknown', senseString(text).type);
     });
 
-    it('should return UNKNOWN for broken XML input', function() {
+    it('should return UNKNOWN for broken XML input', function () {
         assert.equal('unknown', senseString(broken_xml).type);
     });
 
