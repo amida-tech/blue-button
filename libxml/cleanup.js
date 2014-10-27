dff811f331c77900d457853c92b9d09833b65943
Afsin Ustundag <afsin.ustundag@us.pwc.com>
2014-10-27 11:33:59 -0400

    move xml parsing commonalities to livxml

---
+++ libxml/cleanup.js
@@ -0,0 +1,48 @@
+"use strict";
+
+
+var common = require("./common");
+var Processor = require("./processor");
+
+var _ = require("underscore");
+
+var ComponentInstance = require("./componentInstance");
+
+var Cleanup = module.exports = {};
+
+Cleanup.clearNulls = function () {
+
+    //Purges empty objects from arrays, added to improve phone/email support.
+    for (var obj in this.js) {
+        if (_.isArray(this.js[obj])) {
+            for (var jsobj in this.js[obj]) {
+                if (_.isArray(this.js[obj][jsobj].js) === false) {
+                    if (_.isEmpty(this.js[obj][jsobj].js) && _.isUndefined(this.js[obj][jsobj].js) === false) {
+                        delete this.js[obj][jsobj];
+                    }
+                }
+            }
+        }
+    }
+
+    if (!this.js || this.js === null) {
+        return;
+    }
+
+    if ('object' === typeof this.js) {
+        Object.keys(this.js).forEach(function (k) {
+            if ((this.js[k] === null) ||
+                Array.isArray(this.js[k]) && (this.js[k].length === 0 || this.js[k].filter(function (v) {
+                    return v && v.js !== null;
+                }).length === 0) ||
+                this.js[k].js === null) {
+                delete this.js[k];
+            }
+        }, this);
+
+        if (Object.keys(this.js).length === 0) {
+            this.js = null;
+        }
+    }
+
+};
