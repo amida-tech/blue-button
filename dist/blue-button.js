7757f0c09a63ac173b9da61eb130969defcd0410
Afsin Ustundag <afsin.ustundag@us.pwc.com>
2015-01-02 13:42:32 -0500

    version change changes

--- dist/blue-button.js
+++ dist/blue-button.js
@@ -20921,7 +20921,7 @@ function hasOwnProperty(obj, prop) {
 },{}],91:[function(require,module,exports){
 module.exports={
   "name": "blue-button",
-  "version": "1.4.0-beta.1",
+  "version": "1.4.0-beta.2",
   "description": "Blue Button (CCDA, C32, CMS) to JSON Parser.",
   "main": "./index.js",
   "directories": {
