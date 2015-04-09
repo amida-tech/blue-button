dcbed6cd65aab33ef0f38f02f5557744781b175f
Jacob Sachs <jacob.s.sachs@gmail.com>
2015-04-09 14:16:12 -0500

    TDD setup for NCPDP

---
+++ test/parser-ncpdp/test-parser-ncpdp.js
@@ -0,0 +1,36 @@
+var expect = require('chai').expect;
+var fs = require('fs');
+var path = require('path');
+
+var bb = require('../../index.js');
+
+describe('parser.js', function () {
+    var xmlfile = null;
+
+    before(function (done) {
+        xmlfile = fs.readFileSync(__dirname + '/../fixtures/parser-ncpdp/newrx.xml', 'utf-8').toString();
+        done();
+    });
+
+    it('NCPDP parser/model validation', function (done) {
+        expect(xmlfile).to.exist;
+
+        //convert string into JSON
+        var result = bb.parse(xmlfile);
+        expect(result).to.exist;
+
+        //console.log(JSON.stringify(result.data.providers, null, 10));
+
+        var valid = bb.validator.validateDocumentModel(result);
+
+        //if validation failed print all validation errors and summary by category of error
+        if (!valid) {
+            console.log("Errors: \n", JSON.stringify(bb.validator.getLastError(), null, 4));
+        }
+
+        expect(valid).to.be.true;
+
+        done();
+    });
+
+});
