efe71e7ac24028d2825c2d229a35d95cc2dbcce8
Dmitry Kachaev <dmitry.kachaev@gmail.com>
2017-01-02 16:06:50 -0500

    added unit test for nullFlavor check in race/ethnicity fields

---
+++ test/unit/test-nullflavor.js
@@ -0,0 +1,41 @@
+var expect = require('chai').expect;
+var fs = require('fs');
+
+var bb = require('../../index.js');
+
+describe('nullFlavor testing', function () {
+    var xmlfile = null;
+
+    before(function (done) {
+        xmlfile = fs.readFileSync(__dirname + '/../fixtures/parser-ccda/CCD_1_withNF.xml', 'utf-8').toString();
+        done();
+    });
+
+    it('race and ethnicity', function (done) {
+        expect(xmlfile).to.exist;
+
+        //convert string into JSON 
+        var result = bb.parse(xmlfile);
+        expect(result).to.exist;
+
+        //console.log(JSON.stringify(result.data.header, null, 4));
+        //console.log(result.data.header);
+
+        var valid = bb.validator.validateDocumentModel(result);
+
+        //if validation failed print all validation errors
+        if (!valid) {
+            //console.log(JSON.stringify(result.data.header, null, 4));
+            //console.log(JSON.stringify(result.data.demographics, null, 4));
+            console.log("Errors: \n", JSON.stringify(bb.validator.getLastError(), null, 4));
+        }
+
+        //console.log("Demographics: \n", JSON.stringify(result.data.demographics, null, 4));
+
+
+        expect(valid).to.be.true;
+
+        done();
+    });
+
+});
