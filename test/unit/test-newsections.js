fb906224097e18576cda1d519dba3e96f0951ab4
Dmitry Kachaev <dmitry.kachaev@gmail.com>
2017-01-03 15:25:57 -0500

    implemented new sections "reason for referral" and "hospital discharge instructions" and modified "plan of care" (added instructions)

---
+++ test/unit/test-newsections.js
@@ -0,0 +1,41 @@
+var expect = require('chai').expect;
+var fs = require('fs');
+
+var bb = require('../../index.js');
+
+describe('new sections testing', function () {
+    var xmlfile = null;
+
+    before(function (done) {
+        xmlfile = fs.readFileSync(__dirname + '/../fixtures/parser-ccda/CCD_1_newsections.xml', 'utf-8').toString();
+        done();
+    });
+
+    it('reason for referral & hospital discharge instructions', function (done) {
+        expect(xmlfile).to.exist;
+
+        //convert string into JSON 
+        var result = bb.parse(xmlfile);
+        expect(result).to.exist;
+
+        //console.log(JSON.stringify(result.data.header, null, 4));
+        //console.log(result.data.header);
+
+        var valid = bb.validator.validateDocumentModel(result);
+
+        //if validation failed print all validation errors
+        if (!valid) {
+            //console.log(JSON.stringify(result.data.header, null, 4));
+            //console.log(JSON.stringify(result.data.demographics, null, 4));
+            console.log("Errors: \n", JSON.stringify(bb.validator.getLastError(), null, 4));
+        }
+
+        //console.log("All: \n", JSON.stringify(result, null, 4));
+
+
+        expect(valid).to.be.true;
+
+        done();
+    });
+
+});
