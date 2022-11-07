33636cefc076a1530a0d90318d431773c9cc61a7
Richard Whitney <rwhitney@ktc-ems.com>
2022-11-07 08:00:33 -0700

    add supporting files

--- run.js
+++ run.js
@@ -2,7 +2,7 @@ var fs = require('fs');
 var path = require('path');
 var bb = require('./');
 
-var file = path.join(__dirname, 'test/fixtures/parser-ccda/Transfer_Summary.xml');
+var file = path.join(__dirname, 'test/fixtures/parser-ccda/CCD_1.xml');
 var xml = fs.readFileSync(file, {encoding: 'utf-8'});
 var result = bb.parse(xml);
 
